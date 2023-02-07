b9045173732c2c6de25f56529b2ca58b38edb3db
JohnWick08 <mgao041@uottawa.ca>
2023-02-07 15:28:37 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,28 @@ function openInfo(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+function checkIt(){
+    var form = document.getElementById("myForm");
+    //alert(checkForm(form));
+    if(checkForm(form)){
+        alert("submission successful!");
+        openInfo(event, 'services');
+    }
+}
+
+//check if the form is filled
+function checkForm(form) {
+    // get all the inputs within the submitted form
+    var inputs = form.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        // only validate the inputs that have the required attribute
+        if (inputs[i].hasAttribute("required")) {
+            if (inputs[i].value == "") {
+                // found an empty field that is required
+                alert("Please fill all required fields");
+                return false;
+            }
+        }
+    }
+    return true;
+}
